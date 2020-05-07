1b075d9372738c82ce5bb3a00bb66abcd4756d23
unknown <joshthurston92@gmail.com>
2020-05-07 01:51:50 -0600

    kick self functionality

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,15 @@
 const client = require('../client/client')
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on("message", async (msg) => {
+  if (msg.content === "!ping") {
+      const member = msg.guild.member(msg.author)
+      try {
+          let goodbye = await member.kick()
+          if (goodbye) return msg.reply('Bye bye')
+      } catch (err) {
+          return msg.reply('Not enough power')
+      }
+
   }
 });
 
